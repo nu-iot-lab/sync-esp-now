278557398287aa8c9b60d433f831d33f07735df0
Dimitrios Zorbas <zorbasj@gmail.com>
2023-11-23 19:24:13 +0600

    new version for master

--- 2hop/gateway/gateway.ino
+++ 2hop/gateway/gateway.ino
@@ -2,22 +2,18 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
+#include <random>
 
 #define TIME_PERIOD 5
 #define S_TO_MS 1000
 #define MAX_TX_COUNT 500
 #define SPECIAL_COUNT 10
-#define MS_TO_S 1000
-#define MIN_TO_S 60
 
 #define RETR_NUM_K 1
 #define HOP_NUM_N 2
-
-// MAC Address of responder/receiver/slave - edit as required
-// addr of r1 = 0x0C,0xB8,0x15,0xD7,0x76,0xD4
+#define TX_DELAY 1
 
 uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-unsigned int packetNumber = 1;
 
 wifi_country_t country = {
   .cc = "JP",
@@ -30,23 +26,19 @@ wifi_country_t country = {
 // Info about the peer
 esp_now_peer_info_t slaveInfo;
 
-// Define a data structure
-typedef struct struct_message {
-  // unsigned int time;
-  unsigned int packetNumber;
-  unsigned int time_to_live;
+struct packet{
+  unsigned short int packetNumber;
+  uint8_t ttl;
+  uint8_t id;
   float dist1;
   float time1;
-  float dist2;
-  float time2;
-  float dist3;
-  float time3;
-  float dist4;
-  float time4;
-} struct_message;
+  char lat;
+  char lon;
+  unsigned int CRC;
+//  uint8_t pad;
+};
 
-// Create a structured object
-struct_message myData;
+packet p;
 
 void setup() {
   // Set up Serial Monitor
@@ -81,91 +73,46 @@ void setup() {
     return;
   }
   slaveInfo.channel = 14;
-  // delay(5* MIN_TO_S * MS_TO_S);
 
-  myData.dist1 = 1.0;
-  myData.time1 = 1.0;
-  myData.dist2 = 2.0;
-  myData.time2 = 2.0;
-  myData.dist3 = 3.0;
-  myData.time3 = 3.0;
-  myData.dist4 = 4.0;
-  myData.time4 = 4.0;
-  myData.time_to_live = HOP_NUM_N;
+  p.packetNumber = 0;
+  p.ttl = HOP_NUM_N;
+  p.id = 1;
+  p.dist1 = 3000*rand()/1000;
+  p.time1 = p.dist1/16.6667;
+  p.lat = 'N';
+  p.lon = 'W';
+  p.CRC = 2565615138;
+//  p.pad = 0x0;
 }
 
 // ------- LOOP --------------------------------------------------
 
-int tx = 0;
-int stx = 0;
-
 void loop() {
-  while (tx < MAX_TX_COUNT) {
-  // while (1) {
-    // Format structured data
-    // myData.time = 24; // put time here
-    myData.packetNumber = packetNumber;
-
-    packetNumber++;
+  while (p.packetNumber < MAX_TX_COUNT) {
 
     Serial.print("\nSENDING ...\n");
-    // Serial.printf("%lu\t", millis());
-    // esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-    // delay(1);
-
-    // esp_err_t result2 = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-    // delay(1);
-
-    // esp_err_t result3 = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-    // Serial.printf("%lu\n", millis());
     for (int i=0; i<RETR_NUM_K; i++){
-      esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-      delay(1);
+      esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&p, sizeof(p));
+      delay(TX_DELAY);
     }
 
+    // waiting time: period - propagation time - time between transmissions
+    float del = RETR_NUM_K*sizeof(p)*8.0/250 + RETR_NUM_K*TX_DELAY*1.0;
+    delay(TIME_PERIOD * S_TO_MS - del);
 
-    myData.dist1++;
-    myData.time1++;
-    myData.dist2++;
-    myData.time2++;
-    myData.dist3++;
-    myData.time3++;
-    myData.dist4++;
-    myData.time4++;
-
-    // // waiting time...
-    delay(TIME_PERIOD * S_TO_MS - RETR_NUM_K);
-    // delay(TIME_PERIOD * S_TO_MS);
-
-    tx++;
+    p.packetNumber++;
   }
-
+  
+  int stx = 0;
   while (stx < SPECIAL_COUNT) {
-    stx++;
-
-    // Format structured data
-    // myData.time = 0; // put time here
-    myData.packetNumber = 0;
 
     Serial.print("\n SENDING SPECIAL packet ...\n");
-    // esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-    // delay(1);
-
-    // esp_err_t result2 = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-    // delay(1);
-
-    // esp_err_t result3 = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
 
     for (int i=0; i<RETR_NUM_K; i++){
-      esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&myData, sizeof(myData));
-      delay(1);
+      esp_err_t result = esp_now_send(slaveInfo.peer_addr, (uint8_t *)&p, sizeof(p));
+      delay(TX_DELAY);
     }
-
-    // waiting time...
-
-    if (stx != SPECIAL_COUNT) {
-      delay(TIME_PERIOD * S_TO_MS - RETR_NUM_K);
-    }
+    stx++;
   }
 
   Serial.println("Turning off...");
